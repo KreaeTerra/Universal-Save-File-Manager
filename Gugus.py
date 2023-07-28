535798099b24c2e8faef88e24416ff70c16f167a
KreaeTerra <tugsancitir@hotmail.com>
2023-07-29 01:14:28 +0300

    Added entries and labes for filepaths

--- Gugus.py
+++ Gugus.py
@@ -3,10 +3,13 @@ import tkinter as tk
 from shutil import copy2
 
 Window = tk.Tk()
+Window.title("Save File Manager")
+Window.geometry("300x50")
 
-my_label = tk.Label(text= "Gugus").grid(row=0, column=0)
-
-
+source_label = tk.Label(text= "Source").grid(row=0, column=0)
+source_entry = tk.Entry().grid(row=0, column=1)
+dest_label = tk.Label(text= "Destination").grid(row=1, column=0)
+dest_entry = tk.Entry().grid(row=1, column=1)
 
 
 tk.mainloop()
