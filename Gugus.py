6ea073909ede35fd4d1bab8cf504b18f53075a85
KreaeTerra <tugsancitir@hotmail.com>
2023-07-29 01:30:42 +0300

    I have no idea lmao

--- Gugus.py
+++ Gugus.py
@@ -2,18 +2,30 @@
 import tkinter as tk
 from shutil import copy2
 
+
+
+
 #initialization and configuration of the main window
 Window = tk.Tk()
 Window.title("Save File Manager")
-Window.geometry("300x50")
+Window.geometry("300x80")
 
 #creating the filepath entry boxes and their labels
-source_label = tk.Label(text= "Source").grid(row=0, column=0)
-source_entry = tk.Entry().grid(row=0, column=1)
-dest_label = tk.Label(text= "Destination").grid(row=1, column=0)
-dest_entry = tk.Entry().grid(row=1, column=1)
+source_label = tk.Label(root, text= "Source").grid(row=0, column=0)
+source_entry = tk.Entry(root).grid(row=0, column=1)
+dest_label = tk.Label(root, text= "Destination").grid(row=1, column=0)
+dest_entry = tk.Entry(root).grid(row=1, column=1)
 
 
+def SaveFile(source_entry, dest_entry):
+    source = source_entry.get()
+    dest = dest_entry.get()
+
+    print(source, dest)
+
+
+
+save_button = tk.Button(root, text= "Save", command=SaveFile).grid(row=2, column=1)
 
 
 tk.mainloop()
