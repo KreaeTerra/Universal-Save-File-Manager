ba50870937bfa3755b267130b000eb079acf7b16
KreaeTerra <tugsancitir@hotmail.com>
2023-07-29 11:35:53 +0300

    Seperated the creation&placing of objects and implemented a basic save function

--- Gugus.py
+++ Gugus.py
@@ -10,22 +10,30 @@ Window = tk.Tk()
 Window.title("Save File Manager")
 Window.geometry("300x80")
 
+
 #creating the filepath entry boxes and their labels
-source_label = tk.Label(root, text= "Source").grid(row=0, column=0)
-source_entry = tk.Entry(root).grid(row=0, column=1)
-dest_label = tk.Label(root, text= "Destination").grid(row=1, column=0)
-dest_entry = tk.Entry(root).grid(row=1, column=1)
+source_label = tk.Label(text= "Source")
+source_label.grid(row=0, column=0)
+
+source_entry = tk.Entry(width= 35)
+source_entry.insert(0, r"C:\Users\tugsa\Desktop\Saitama.plr")
+source_entry.grid(row=0, column=1)
+
+dest_label = tk.Label(text= "Destination")
+dest_label.grid(row=1, column=0)
+
+dest_entry = tk.Entry(width= 35)
+dest_entry.insert(0, r"C:\Users\tugsa\Desktop\test")
+dest_entry.grid(row=1, column=1)
 
 
-def SaveFile(source_entry, dest_entry):
-    source = source_entry.get()
-    dest = dest_entry.get()
 
-    print(source, dest)
+def SaveFile():
+    copy2(str(source_entry.get()), str(dest_entry.get()))
 
 
 
-save_button = tk.Button(root, text= "Save", command=SaveFile).grid(row=2, column=1)
+save_button = tk.Button(text= "Save", command= SaveFile).grid(row=2, column=1)
 
 
 tk.mainloop()
